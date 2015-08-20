69847070c57cc53b65d67f5593700d4b67a17290
Lucas S. Silveira <lssilveira11@gmail.com>
2015-08-20 03:49:33 -0300

    Add Friend function

--- app/controllers/Amigo.java
+++ app/controllers/Amigo.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
 import play.data.DynamicForm;
 import play.mvc.*;
 import play.db.jpa.*;
@@ -13,21 +14,56 @@ import static play.libs.Json.*;
 public class Amigo extends Controller {
 
     public Result index() {
-        return ok(amigo.render());
+
+        Long idPessoa = Long.parseLong(session().get("conectedId"));
+
+        List<Pessoa> pessoas = Ebean.find(Pessoa.class)
+                                .where().ne("id", idPessoa)
+                                .findList();
+
+        return ok(amigo.render(pessoas));
     }
 
     public Result addAmigo(String idUser, String idAmigo){
 
-        String msg;
+        String msg = "ok";
 
-        if (idUser != null && idAmigo != null) { //TODO: isFriend == null
-            //TODO: search if they are friends
-            //TODO: persist new friend
-            msg = "ok";
+        if (idUser != null && idAmigo != null) {
+            // search if they are friends
 
-        } else {//TODO: if(isFriend != null)
-            msg = "remove";
-            //TODO: persist remove friend
+            Long idPessoa1 = Long.parseLong(idUser);
+            Pessoa p1 = Pessoa.getById(idPessoa1);
+
+            Long idPessoa2 = Long.parseLong(idAmigo);
+            Pessoa p2 = Pessoa.getById(idPessoa2);
+
+            boolean alreadyFriends = false;
+            for(Pessoa p : p1.getAmigos()){
+                if (p.getId().equals(p2.getId())) {
+                    alreadyFriends = true;
+                    break;
+                }
+            }
+
+            if (!alreadyFriends) {
+                // persist new friend
+                p1.getAmigos().add(p2);
+                p2.getAmigos().add(p1);
+
+                p1.save();
+                p2.save();
+
+                msg = "ok";
+            } else {
+                // persist remove friend
+                p1.getAmigos().remove(p1.getAmigos().indexOf(p2));
+                p2.getAmigos().remove(p2.getAmigos().indexOf(p1));
+
+                p1.save();
+                p2.save();
+
+                msg = "remove";
+            }
         }
 
         return ok(msg);
